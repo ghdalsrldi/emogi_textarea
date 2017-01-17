0f61fecb25e2c9926b6e8e734a079e0ddc52f8f7
mkhong <aksen1>
2017-01-17 13:36:53 +0900

    create emogi_textarea.js

---
+++ js/emogi_textarea.js
@@ -0,0 +1,66 @@
+/**
+ * Created by mkhong on 2017-01-17.
+ */
+
+(function($, window, document){
+    var pasteHtmlAtCaret = function (html, selectPastedContent) {
+        var sel, range;
+        if (window.getSelection) {
+            // IE9 and non-IE
+            sel = window.getSelection();
+            if (sel.getRangeAt && sel.rangeCount) {
+                range = sel.getRangeAt(0);
+                range.deleteContents();
+
+                // Range.createContextualFragment() would be useful here but is
+                // only relatively recently standardized and is not supported in
+                // some browsers (IE9, for one)
+                var el = document.createElement("div");
+                el.innerHTML = html;
+
+                var frag = document.createDocumentFragment(), node, lastNode;
+
+
+                while ( (node = el.firstChild) ) {
+                    lastNode = frag.appendChild(node);
+                }
+                var firstNode = frag.firstChild;
+                range.insertNode(frag);
+
+                // Preserve the selection
+                if (lastNode) {
+                    range = range.cloneRange();
+                    range.setStartAfter(lastNode);
+                    if (selectPastedContent) {
+                        range.setStartBefore(firstNode);
+                    } else {
+                        range.collapse(true);
+                    }
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                }
+            }
+        } else if ( (sel = document.selection) && sel.type != "Control") {
+            // IE < 9
+            var originalRange = sel.createRange();
+            originalRange.collapse(true);
+            sel.createRange().pasteHTML(html);
+            if (selectPastedContent) {
+                range = sel.createRange();
+                range.setEndPoint("StartToStart", originalRange);
+                range.select();
+            }
+        }
+    };
+
+    var emogi_input = function (obj)
+    {
+        var input = '<img src="'+$(obj).find('img').attr('src')+'" width="40">';
+
+        document.getElementById("edit").focus();
+        pasteHtmlAtCaret(input, false);
+    };
+    $.emogi_input = emogi_input;
+})(jQuery, window, document);
+
+
